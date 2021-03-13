a4e6e1892bd5e5864b64ed57ff535c10b1141b5c
FabianoEtec <nbscbs@gmail.com>
2021-03-13 12:07:23 -0300

    Programa digite uma frase ao cebolinha finalizado!!

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace amigocebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("╔═════════════════════════════════════════╗");
+            Console.WriteLine("║ M E U    A MI G O    C E B O L I N H A  ║");
+            Console.WriteLine("╚═════════════════════════════════════════╝");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Digite uma frase para seu amiguinho: ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            string cebolinha = Console.ReadLine();
+            string textoDigitado = cebolinha.Replace("r","l");
+            textoDigitado = textoDigitado.Replace("R","L");
+            Console.WriteLine(textoDigitado);
+            Console.ResetColor();
+           
+
+            
+        }
+    }
+}
